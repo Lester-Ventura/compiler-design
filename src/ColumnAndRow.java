ce1102ce4deb1ace21bf281e2ceed6a81bd9dfe1
sonedayy <149049974+sonedayy@users.noreply.github.com>
2025-03-03 01:13:36 +0800

    Update: added getRow & getColumn in case debugging is important
    - we can just add 1 to row and column number for accurate debugging since I think the scanner is zero-based (will only affect error message)
    - tried modifying the scanner to start at one for lines & column but will result to out of bound error, so did not continue it

--- src/ColumnAndRow.java
+++ src/ColumnAndRow.java
@@ -3,16 +3,26 @@ public class ColumnAndRow {
   public int row;
 
   public ColumnAndRow(int row, int column) {
-    this.column = column;
-    this.row = row;
+      this.column = column;
+      this.row = row;
+  }
+
+  // we add one to getRow and getColumn if debugging is important, but will not be consistent to orig numbering
+  public int getRow() {
+      return row; 
+  }
+
+  public int getColumn() {
+      return column; 
   }
 
   public static ColumnAndRow calculate(int index, String source) {
-    String[] lines = source.split("\n", -1);
-    int currentLine = 0;
+      String[] lines = source.split("\n", -1);
+      int currentLine = 0;
+      int column = index;
 
-    for (; index > lines[currentLine].length(); index -= lines[currentLine++].length() + 1)
-      ;
-    return new ColumnAndRow(currentLine, index);
+      for (; column > lines[currentLine].length(); column -= lines[currentLine++].length() + 1);
+
+      return new ColumnAndRow(currentLine, column);
   }
 }
